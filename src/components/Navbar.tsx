8a339484008637e818050bef3e6b6ab9b025e680
Simeon Kauffmann <simeon.kauffmann@gmail.com>
2021-12-03 10:16:33 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,13 @@
-import React from "react"
+
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  return <div></div>
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <Link to='/login'>Login</Link>
+      <Link to='/list'>List</Link>
+      <Link to='/cart'>Cart</Link>
+    </div>
+  );
 }
