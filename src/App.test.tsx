aa0020644f8ba14e072d38e158a84c8ea7a48a98
SimeonKauffmann <simeon.kauffmann@gmail.com>
2021-12-07 10:12:26 +0100

    'changes'

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,5 @@ import React from "react"
 import { shallow } from "enzyme"
 
 test("renders learn react link", () => {
-  render(<App />)
   expect(2 + 2).toBe(4)
 })
--- src/routing.jsx
+++ src/routing.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Cart from './views/CartView.tsx';
-import List from './views/ListView';
-import Login from './views/LoginView';
-import Product from './views/ProductView';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+import Cart from "./views/CartView.tsx"
+import List from "./views/ListView"
+import Login from "./views/LoginView"
+import Navbar from "./components/Navbar"
+import Product from "./views/ProductView"
+import React from "react"
 
 const Routing = () => {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path='/cart' component={Cart} />
-        <Route exact path='/list' component={List} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/product/:id' component={Product} />
-      </Switch>
+      <Routes>
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/list" component={List} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/product/:id" component={Product} />
+      </Routes>
     </Router>
-  );
-};
-export default Routing;
+  )
+}
+export default Routing
